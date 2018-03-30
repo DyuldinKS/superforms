7f3463ba6170f8f3890ae2f5cdc25c3aa4eb7d46
DyuldinKS <dyuldin_kirill@protonmail.com>
2018-03-30 12:37:10 +0300

    fix sequence restarting bug

--- src/server/db/init.sql
+++ src/server/db/init.sql
@@ -1,25 +1,34 @@
 INSERT INTO roles(name)
 VALUES ('root'), ('admin'), ('user'), ('respondent');
 
+
 -- create @system as root of organization tree
 -- create @bot as first user
 WITH
+	ids AS (
+		SELECT nextval('recipients_id_seq') AS user_id,
+			nextval('recipients_id_seq') AS org_id
+	),
 	recipients_i AS (
-		INSERT INTO recipients(email, type, author_id)
-		VALUES ('@bot', 'user', 1), ('@system', 'org', 1)
+		INSERT INTO recipients(id, email, type, author_id)
+		SELECT * FROM (
+			SELECT user_id, '@bot', 'user'::rcpt_type, user_id FROM ids
+			UNION ALL
+			SELECT org_id, '@system', 'org'::rcpt_type, user_id FROM ids
+		) AS user_and_org
 	),
 	org_i AS (
 		INSERT INTO organizations(id, info)
-		VALUES(2, '{}'::jsonb)
+		SELECT org_id, '{}'::jsonb FROM ids
 	)
 INSERT INTO users(id, org_id, info, role_id, hash)
-VALUES(
-	1,
-	2,
+SELECT
+	user_id,
+	org_id,
 	'{}'::jsonb,
 	(SELECT min(id) FROM roles),
 	'$2a$10$k7Px9a/ynquiPs9zpg8aeuGboyvw84T7NFOGNUagH6DbHaepnxDNa' -- justdoit
-);
+FROM ids;
 
 -- show login and password
 SELECT '@bot' AS "login", 'justdoit' AS password;
