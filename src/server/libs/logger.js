b200c785163dd5af0175c6fd8bdf88aae981e82a
DyuldinKS <dyuldin_kirill@protonmail.com>
2018-04-12 19:27:47 +0300

    add log file rotation

--- src/server/libs/logger.js
+++ src/server/libs/logger.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import bunyan from 'bunyan';
+import rotatingFileStream from 'rotating-file-stream';
 
 
 const NODE_ENV = process.env.NODE_ENV;
@@ -22,7 +23,7 @@ const devSerializers = {
 			: res
 	),
 
-	err: (err) => ({
+	err: err => ({
 		...err,
 		message: err.message,
 		name: Object.getPrototypeOf(err).name,
@@ -79,15 +80,50 @@ const logger = bunyan.createLogger({
 });
 
 
+const stream = rotatingFileStream(
+	'info.log',
+	{
+		path: path.join(__dirname, 'logs'),
+		initialRotation: true,
+		interval: '1d',
+		rotate: 1,
+		compress: 'gzip', // compress rotated files
+	},
+);
+
+
 if(NODE_ENV === 'production') {
 	logger.addStream({
-		type: 'rotating-file',
+		type: 'stream',
 		level: 'info',
-		path: path.join(__dirname, '../logs/info.log'),
-		period: '1d',
-		count: 10,
+		stream,
 	});
 }
+
+
+// here are reported blocking errors
+// once this event is emitted, the stream will be closed as well
+stream.on('error', (err) => { logger.error('Rotation error:', err); });
+
+// no rotated file is open (emitted after each rotation as well)
+// filename: useful if immutable option is true
+stream.on('open', (filename) => {	logger.info('Open rotated file:', filename); });
+
+// rotation job removed the specified old rotated file
+// number == true, the file was removed to not exceed maxFiles
+// number == false, the file was removed to not exceed maxSize
+stream.on('removed', (filename, number) => {
+	logger.info('Rotated file removed:', filename, number);
+});
+
+// rotation job started
+stream.on('rotation', () => { logger.info('Rotation started'); });
+
+// rotation job completed with success producing given filename
+stream.on('rotated', (filename) => { logger.info('File rotated:', filename); });
+
+// here are reported non blocking errors
+stream.on('warning', (err) => { logger.warning(err); });
 
 
 export default logger;
