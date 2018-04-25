74e7412d38fb3cb56ef169f4af42257d4e1b6e7d
Vyacheslav Khanilo <khaniloslava@yandex.ru>
2018-04-25 15:30:53 +0300

    refactor generator save panel

--- src/client/apps/app/features/form/generator/components/SavePanel.jsx
+++ src/client/apps/app/features/form/generator/components/SavePanel.jsx
@@ -1,42 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'reactstrap';
+import Moment from 'moment';
 
 const propTypes = {
   hasUnsavedChanges: PropTypes.bool,
+  lastSave: PropTypes.string,
   onSave: PropTypes.func,
   updating: PropTypes.bool,
 };
 
 const defaultProps = {
   hasUnsavedChanges: false,
+  lastSave: '',
   onSave: () => {},
   updating: false,
 };
 
-function SavePanel(props) {
-  const { hasUnsavedChanges, onSave, updating } = props;
+const dateFormat = 'DD.MM.YYYY';
+const timeFormat = 'HH:mm';
 
-  return (
-    <div className="form-generator-save-panel">
-      {
-        updating
-        ? <p className="text-info">
-            Сохранение...
-          </p>
-        : hasUnsavedChanges
-          ? <Button
-              color="primary"
-              onClick={onSave}
-            >
-              Сохранить
-            </Button>
-          : <p className="text-success">
-              Все изменения сохранены
-            </p>
-      }
-    </div>
-  );
+class SavePanel extends Component {
+  renderHasChanges() {
+    const { onSave } = this.props;
+
+    return (
+      <React.Fragment>
+        <p className="text-muted">
+          Есть несохраненные изменения{' '}
+        </p>
+        <Button color="link" onClick={onSave} >
+          Сохранить
+        </Button>
+      </React.Fragment>
+    );
+  }
+
+  renderPending() {
+    return (
+      <p className="text-muted">
+        Сохранение...
+      </p>
+    );
+  }
+
+  renderSuccess() {
+    const { lastSave } = this.props;
+    const date = Moment(lastSave).format(`${dateFormat} ${timeFormat}`);
+
+    return (
+      <p className="text-muted">
+        {`Все изменения сохранены (${date})`}
+      </p>
+    );
+  }
+
+  render() {
+    const { hasUnsavedChanges, updating } = this.props;
+
+    return (
+      <div className="form-generator-save-panel">
+        {
+          updating
+          ? this.renderPending()
+          : hasUnsavedChanges
+            ? this.renderHasChanges()
+            : this.renderSuccess()
+        }
+      </div>
+    );
+  }
 }
 
 SavePanel.propTypes = propTypes;
