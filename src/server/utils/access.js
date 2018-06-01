5411f8b51b75074f6745479cb5472c34b9b59648
DyuldinKS <dyuldin_kirill@protonmail.com>
2018-06-01 12:12:30 +0300

    create access module

--- src/server/utils/access.js
+++ src/server/utils/access.js
@@ -1,156 +1,29 @@
-class Access {
-	constructor(user) {
-		this.subj = user;
-	}
+import buildRoles from './roles';
+import rules from './accessRules';
+import Org from '../models/Org';
+import User from '../models/User';
+import Form from '../models/Form';
 
-	setAction(action) {
-		this.action = action;
-		return this;
-	}
+const roles = buildRoles(rules);
 
-	setCategory(category) {
-		this.category = category;
-		return this;
-	}
+const defineType = obj => obj.constructor.name.toLowerCase();
 
-	setObject(obj) {
-		this.obj = obj;
-		return this.check();
-	}
-
-	in(orgId) {
-		this.orgId = orgId;
-		return this.check();
-	}
-
-
-	check() {
-		if(!this.action) throw new Error('Access check: undefined action');
-		if(!this.category) throw new Error('Access check: undefined category');
-		if(!(this.obj || this.orgId)) {
-			throw new Error('Access check: undefined organization or object');
-		}
-
-		if(this.obj) {
-			// access to a specific obj
-			return this.checkAccessToObject();
-		}
-		// access for a set of objs within an organization
-		return this.checkAccessWithinOrg();
-	}
-
-
-	checkAccessToObject() {
-		const {
-			subj,
-			category,
-			action,
-			obj,
-		} = this;
-		const scope = subj.role.getScope(category, action, 'full');
-
-		return Promise.resolve()
-			.then(() => {
-				if(!scope) return false;
-
-				switch (scope) {
-				case 'local': return subj.orgId === obj.orgId;
-				case 'enclosing': {
-					return subj.org.hasChild(subj.orgId)
-						.then(found => (found !== undefined));
-				}
-				case 'global': return true;
-				default: return false;
-				}
-			});
-	}
-
-
-	checkAccessWithinOrg() {
-		const {
-			subj,
-			category,
-			action,
-			orgId,
-		} = this;
-		const scope = subj.role.getScope(category, action, 'full');
-
-		return Promise.resolve()
-			.then(() => {
-				if(!scope) return false;
-
-				switch (scope) {
-				case 'local': return subj.orgId === orgId;
-				case 'enclosing': {
-					return subj.org.hasChild(orgId)
-						.then(found => (found !== undefined));
-				}
-				case 'global': return true;
-				default: return false;
-				}
-			});
-	}
-}
-
-
-// ***************** PROPERTIES ***************** //
-
-Rights.actions.forEach((i, action) => {
-	Object.defineProperty(
-		Access.prototype,
-		action,
-		{ get: function () { return this.setAction(action); } },
-	);
-});
-
-
-Role.categories.forEach((category) => {
-	Object.defineProperties(
-		Access.prototype,
-		{
-			[category]: {
-				get: function setCategory() {
-					return this.setCategory(category);
-				},
+const can = (user) => {
+	if(!user.can) {
+		user.can = new Proxy(
+			{},
+			{
+				get: (o, action) => (
+					(obj, params) => (
+						roles[user.role][action][defineType(obj)](user, obj, params)
+					)
+				),
 			},
-			[category.slice(0, -1)]: {
-				value: function (obj) {
-					return this.setCategory(category).setObject(obj);
-				},
-			},
-		},
-	);
-});
+		);
+	}
+
+	return user.can;
+};
 
 
-export default Access;
-
-
-// const subj = {
-// 	orgId: 89,
-// 	role: new Role({
-// 		label: 'imc-methodist',
-// 		info: { description: 'this is methodist test role' },
-// 		rights: {
-// 			organizations: 'nlnnl',
-// 			users: 3002,
-// 			forms: 'lpppp',
-// 			responses: 'ppppn',
-// 		},
-// 	}),
-// };
-
-// const can = new Access(subj);
-// const obj = { orgId: 89 };
-
-// console.log(Access.prototype)
-// const can1 = new Access('go fuck yourself bitch!')
-// can.forms
-// console.log(can)
-// can1.update
-// console.log(can)
-// can.create.users.in(obj.orgId)
-// can.read.user(obj)
-// 	.then(console.log)
-// 	.catch(console.log)
-// console.log(proxy.read)
+export { can };
