b9a129b7b02bf99b29bb81940769152a4549fe08
Vyacheslav Khanilo <khaniloslava@yandex.ru>
2018-06-13 17:39:56 +0300

    add preload redux store middleware for ssr

---
+++ src/server/middleware/preloadReduxStore.js
@@ -0,0 +1,25 @@
+import createStore from '../../client/apps/app/boot/createStore';
+import { actions as routerActions } from '../../client/shared/router/redux';
+import { actions as sessionActions } from '../../client/apps/app/shared/redux/session';
+
+// load common ssr redux data to store
+export default (req, res, next) => {
+	const store = createStore();
+	const user = req.author;
+	console.log(user);
+
+	try {
+		store.dispatch(routerActions.init(req.url));
+		store.dispatch(sessionActions.init(
+			`${user.id}`,
+			`${user.org.id}`,
+			user.toStore(),
+			user.org.toStore(),
+		));
+	} catch (error) {
+		throw error;
+	}
+
+	req.reduxStore = store;
+	next();
+};
