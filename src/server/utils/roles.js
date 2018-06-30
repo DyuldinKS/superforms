199bc6a4f2ac023f9acc6e938672aeed0dc40267
DyuldinKS <dyuldin_kirill@protonmail.com>
2018-06-30 17:52:56 +0300

    update roles: implement checking access to forms and responses; modify structure of 3rd access param; some logical fixes.

--- src/server/utils/roles.js
+++ src/server/utils/roles.js
@@ -2,8 +2,13 @@ const buildRoles = ({
 	// subject-object relationship rules
 	isSameUser,
 	isSameOrg,
-	isOwner,
 	isFormOwner,
+	// object rules
+	isFormUnsent,
+	isFormActive,
+	isFormOfResponseActive,
+	isResponseToSharedForm,
+	isResponseToPersonalizedForm,
 	// scope rules
 	nowhere,
 	withinOrg,
@@ -18,108 +23,118 @@ const buildRoles = ({
 	// *********************************  ROOT  ******************************* //
 	root: {
 		create: {
-			// create organization with it's parent in subtree
-			org: (subj, org, params) => (
+			// create organization is his org subtree
+			org: (subj, org, { body }) => (
 				inSubtree(subj, org)
-					&& areEqualSets(params, ['email', 'parentId', 'info'])
+					&& areEqualSets(body, ['email', 'parentId', 'info'])
 			),
 
-			user: (subj, user, params) => (
+			user: (subj, user, { body }) => (
 				inSubtree(subj, user)
-					&& areEqualSets(params, ['email', 'orgId', 'info', 'role'])
+					&& areEqualSets(body, ['email', 'orgId', 'info', 'role'])
 			),
 
-			form: (subj, form, params) => (
-				areEqualSets(params, ['title', 'description', 'scheme'])
+			form: (subj, form, { body }) => (
+				isSubset(body, ['title', 'description', 'scheme'])
+					&& isSubset(['title', 'description'], body)
 			),
 
-			response: everywhere,
+			response: (subj, response) => (
+				isFormOfResponseActive(subj, response)
+					&& isResponseToSharedForm(subj, response)
+			),
 		},
 
 		read: {
-			org: (subj, org) => inSubtree(subj, org),
-			user: (subj, user) => inSubtree(subj, user),
-			form: (subj, form) => inSubtree(subj, form),
-			response: (subj, response) => (
-				inSubtree(subj, response) || isOwner(subj, response)
-			),
+			org: inSubtree,
+			user: inSubtree,
+			form: inSubtree,
+			response: inSubtree,
 		},
 
 		update: {
-			org: (subj, org, props) => (
-				( // update another organization
-					inSubtree(subj, org) && !isSameOrg(subj, org)
-						&& isSubset(props, ['email', 'active', 'info'])
-				)
-				|| ( // update his org
-					isSameOrg(subj, org) && isSubset(props, ['email', 'info'])
-				)
+			org: (subj, org, { body }) => (
+				// updates another organization in subtree
+				(inSubtree(subj, org) && !isSameOrg(subj, org)
+					&& isSubset(body, ['email', 'active', 'info', 'deleted']))
+				// updates his org
+				|| (isSameOrg(subj, org) && isSubset(body, ['email', 'info']))
 			),
 
-			user: (subj, user, props) => (
-				( // update another user in subtree
-					inSubtree(subj, user) && !isSameUser(subj, user)
-						&& isSubset(props, ['email', 'active', 'info', 'role', 'password'])
-				)
-				|| ( // or update himself
-					isSameUser(subj, user) && isSubset(props, ['email', 'info', 'password'])
-				)
+			user: (subj, user, { body }) => (
+				// updates another user in subtree
+				(inSubtree(subj, user) && !isSameUser(subj, user))
+				// or updates himself
+				|| (isSameUser(subj, user)
+					&& isSubset(body, ['email', 'info', 'password']))
 			),
 
-			form: (subj, form) => inSubtree(subj, form),
-			response: (subj, response) => isOwner(subj, response),
+			form: inSubtree,
+
+			response: (subj, response, { body }) => (
+				// soft deletion or recovering
+				inSubtree(subj, response) && areEqualSets(body, ['deleted'])
+			),
 		},
 	},
 
+
 	// *********************************  ADMIN  ****************************** //
+
 	admin: {
 		create: {
 			org: nowhere,
 
-			user: (subj, user, params) => (
+			user: (subj, user, { body }) => (
 				withinOrg(subj, user)
-					&& areEqualSets(params, ['email', 'orgId', 'info', 'role'])
-					&& isElemOf(params.role, ['admin', 'user'])
-			),
-			form: (subj, form, params) => (
-				areEqualSets(params, ['title', 'description', 'scheme'])
+					&& areEqualSets(body, ['email', 'orgId', 'info', 'role'])
+					&& isElemOf(body.role, ['admin', 'user'])
 			),
 
-			response: everywhere,
+			form: (subj, form, { body }) => (
+				areEqualSets(body, ['title', 'description', 'scheme'])
+			),
+
+			response: (subj, response) => (
+				isFormOfResponseActive(subj, response)
+					&& isResponseToSharedForm(subj, response)
+			),
 		},
 
 		read: {
-			org: (subj, org) => isSameOrg(subj, org),
-			user: (subj, user) => withinOrg(subj, user),
-			form: (subj, form) => withinOrg(subj, form),
-
-			response: (subj, response) => (
-				withinOrg(subj, response) || isOwner(subj, response)
+			org: (subj, org, { subpath }) => (
+				isSameOrg(subj, org) && !isEqual(subpath, 'orgs/new')
 			),
+			user: withinOrg,
+			form: withinOrg,
+			response: withinOrg,
 		},
 
 		update: {
-			org: (subj, org, props) => (
-				isSameOrg(subj, org) && isSubset(props, ['email', 'info'])
+			org: (subj, org, { body }) => (
+				isSameOrg(subj, org) && isSubset(body, ['email', 'info'])
 			),
 
-			user: (subj, user, props) => (
-				( // update another user in subtree
-					withinOrg(subj, user) && !isSameUser(subj, user)
-						&& isSubset(props, ['email', 'active', 'info', 'role', 'password'])
-				)
-				|| ( // or update himself
-					isSameUser(subj, user) && isSubset(props, ['email', 'info', 'password'])
-				)
+			user: (subj, user, { body }) => (
+				// updates another user in subtree
+				(withinOrg(subj, user) && !isSameUser(subj, user))
+				// or updates himself
+				|| (isSameUser(subj, user)
+					&& isSubset(body, ['email', 'info', 'password']))
 			),
 
-			form: (subj, form, props) => isOwner(subj, form) || withinOrg(subj, form),
+			form: withinOrg,
 
-			response: (subj, response, props) => isOwner(subj, response),
+			response: (subj, response, { body }) => (
+				// soft deletion or recovering
+				withinOrg(subj, response) && areEqualSets(body, ['deleted'])
+			),
 		},
 	},
 
+
 	// *********************************  USER  ******************************* //
+
 	user: {
 		create: {
 			org: nowhere,
@@ -127,30 +142,53 @@ const buildRoles = ({
 			form: (subj, form, params) => (
 				areEqualSets(params, ['title', 'description', 'scheme'])
 			),
-			response: everywhere,
+			response: (subj, response) => (
+				isFormOfResponseActive(subj, response)
+					&& isResponseToSharedForm(subj, response)
+			),
 		},
 
 		read: {
-			org: (subj, org, params = {}) => (
-				isSameOrg(subj, org) && isEqual(params.section, 'forms')
+			org: (subj, org, { subpath }) => (
+				isSameOrg(subj, org) && isEqual(subpath, 'forms')
 			),
-			user: (subj, user, params) => (
-				withinOrg(subj, user) && isElemOf(params.section, ['info', 'forms'])
+			user: (subj, user, { subpath }) => (
+				// reads another user in his org
+				(withinOrg(subj, user) && !isSameUser(subj, user)
+					&& isElemOf(subpath, [undefined, 'info', 'forms']))
+				// reads himself
+				// subpath is undefined for api request to get user
+				|| (isSameUser(subj, user)
+						&& isElemOf(subpath, [undefined, 'info', 'forms', 'settings']))
 			),
-			form: (subj, form, params) => (
-				isOwner(subj, form) || withinOrg(subj, form)
+			form: (subj, form, { subpath }) => (
+				isFormOwner(subj, form)
+					// only preview of foreign forms
+					// subpath === undefined for interview page or api request to get form
+					|| (withinOrg(subj, form) && !isFormOwner(subj, form)
+						&& isSubset(subpath, [undefined, 'preview']))
 			),
-			response: (subj, response, params) => (
-				isOwner(subj, response) || isFormOwner(subj, response)
-					|| withinOrg(subj, response)
-			),
+			response: (subj, response) => isFormOwner(subj, response),
 		},
 
 		update: {
 			org: nowhere,
-			user: nowhere,
-			form: isOwner,
-			response: isOwner,
+			user: (subj, user, { body }) => (
+				// updates himself
+				isSameUser(subj, user) && isSubset(body, ['email', 'info', 'password'])
+			),
+			form: (subj, form, { body }) => (
+				isFormOwner(subj, form)
+					&& (// if form is under developing
+						(isFormUnsent(subj, form) && !isElemOf('ownerId', body))
+						// if responses collecting started
+						|| (!isFormUnsent(subj, form)
+							&& isSubset(body, ['title', 'description', 'collecting'])))
+			),
+			response: (subj, response, { body }) => (
+				// soft deletion or recovering
+				withinOrg(subj, response) && areEqualSets(body, ['deleted'])
+			),
 		},
 	},
 });
