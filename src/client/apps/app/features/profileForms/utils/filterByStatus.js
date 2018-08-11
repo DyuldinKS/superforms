387d4b58262c350ddb07a358df5b0258306750c7
DyuldinKS <dyuldin_kirill@protonmail.com>
2018-08-11 13:58:09 +0300

    change form filters on client side

--- src/client/apps/app/features/profileForms/utils/filterByStatus.js
+++ src/client/apps/app/features/profileForms/utils/filterByStatus.js
@@ -4,13 +4,16 @@ import { constants } from './statusTabs';
 export default function filterByStatus(list, status) {
   switch (status) {
     case constants.ACTIVE:
-      return list.filter(item => item.collecting
-        && !item.collecting.inactive
-        && (!item.collecting.expires || Moment().isBefore(item.collecting.expires)));
+      return list.filter(item => (
+        item.collecting
+          && (!item.collecting.stop || Moment().isBefore(item.collecting.stop))
+      ));
 
     case constants.INACTIVE:
-      return list.filter(item => item.collecting
-        && (!!item.collecting.inactive || Moment().isAfter(item.collecting.expires)));
+      return list.filter(item => (
+        item.collecting && item.collecting.stop
+          && Moment().isAfter(item.collecting.stop)
+      ));
 
     case constants.UNSENT:
       return list.filter(item => !item.collecting);
